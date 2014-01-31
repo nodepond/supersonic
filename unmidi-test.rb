9b8533416d512fe74f2a88bfd0b98da71f9add10
Nodepond <hello@nodepond.com>
2014-01-31 17:33:27 +0100

    added unimidi-test

---
+++ unmidi-test.rb
@@ -0,0 +1,26 @@
+require 'unimidi'
+
+# this program prompts the user to select a midi output and sends some arpeggiated chords to it
+
+notes = [36, 40, 43] # C E G
+octaves = 5
+duration = 0.1
+
+# prompt the user to select an output
+UniMIDI::Output.gets do |output| # using their selection...
+
+  (0..((octaves-1)*12)).step(12) do |oct|
+
+  	#while true
+    	notes.each do |note|
+    	
+      	output.puts(0x90, note + oct, 64) # note on
+      	sleep(duration) # wait
+     	output.puts(0x80, note + oct, 64) # note off
+      
+    	end
+	#end
+    
+  end
+
+end
