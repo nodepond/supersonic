70ccc3c8c9690bb4f980b28851e831355b7382a8
Nodepond <hello@nodepond.com>
2014-01-31 18:36:59 +0100

    first successful irb test

--- unimidi-test.rb
+++ unimidi-test.rb
@@ -1,26 +1,55 @@
+require 'irb'
+require 'irb/completion'
 require 'unimidi'
 
 # this program prompts the user to select a midi output and sends some arpeggiated chords to it
 
-notes = [36, 40, 43] # C E G
-octaves = 5
-duration = 0.1
+@notes = [36, 40, 43] # C E G
+@octaves = 5
+@duration = 0.1
+
+@midiout = nil
 
 # prompt the user to select an output
 UniMIDI::Output.gets do |output| # using their selection...
 
-  (0..((octaves-1)*12)).step(12) do |oct|
+  @midiout = output
+
+  (0..((@octaves-1)*12)).step(12) do |oct|
 
   	#while true
-    	notes.each do |note|
+    	@notes.each do |note|
     	
-      	output.puts(0x90, note + oct, 64) # note on
-      	sleep(duration) # wait
-     	output.puts(0x80, note + oct, 64) # note off
+        @midiout.puts(0x90, note + oct, 64) # note on
+      	sleep(@duration) # wait
+     	  @midiout.puts(0x80, note + oct, 64) # note off
       
     	end
 	#end
     
   end
-
 end
+
+def a 
+  (0..((@octaves-1)*12)).step(12) do |oct|
+
+    @notes.each do |note|
+      @midiout.puts(0x90, note + oct, 64) # note on
+      sleep(@duration) # wait
+      @midiout.puts(0x80, note + oct, 64) # note off
+    end
+  end
+end
+
+def seven
+  (0..((@octaves-1)*12)).step(12) do |oct|
+
+    @notes.each do |note|
+      @midiout.puts(0x90, note + oct + 7, 64) # note on
+      sleep(@duration) # wait
+      @midiout.puts(0x80, note + oct + 7, 64) # note off
+    end
+  end
+end
+
+IRB.start
