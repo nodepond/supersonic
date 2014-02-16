53820a0dc32e4308053e17274c741be47791dcff
Nodepond <hello@nodepond.com>
2014-02-16 20:26:46 +0100

    some sequencer tests and a change, to start pureData instrument from command line without interface

--- arp-test.rb
+++ arp-test.rb
@@ -3,13 +3,13 @@ opts = {
 	:interval => 7,
 	:midi => @midiout,
 	:pattern => Diamond::Pattern["UpDown"],
-	:range => 4, 
+	:range => 2, 
 	:rate => 8
 }
 
-arp = Diamond::Arpeggiator.new(120, opts)
+arp = Diamond::Arpeggiator.new(133, opts)
 
-chords = ["c2","a3"]
+chords = ["c3","a3","d3"]
 
 arp << chords
 
