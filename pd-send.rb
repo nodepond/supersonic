59d157da31276d59ad15d739047db617fcf787fd
Nodepond <hello@nodepond.com>
2014-02-18 22:07:32 +0100

    fleshed out the pd-send example

--- pd-send.rb
+++ pd-send.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'irb'
+require 'irb/completion'
 
 ### UDP Socket connection has to be some troubles. Let's make this later...
 
@@ -8,14 +9,41 @@ require 'irb'
 #s.send('hello World;', 0, 'localhost', 3001)
 #puts s
 
-@sock = TCPSocket.new("localhost",3000)
-@sock.write ( 'hello World2;' )
-@sock.write ( "this is you;" )
 
-def pd(message)
-	@sock.write(message+";")
+module Pd
+
+	@sock = nil
+	
+	def connect(host, port)
+		@sock = TCPSocket.new("localhost",port)
+		@sock.write ( 'SuperSonic inited on port '+ port.to_s)
+	end
+
+	def disconnect
+		@sock.close
+	end
+
+	def send(message)
+		@sock.write(message+";")
+	end
+
+end
+
+class PdSeq16
+	include Pd
+
+	def init(host='localhost',port=3000)
+		self.connect(host,port)
+	end
+end
+
+@pdseq16 = PdSeq16.new
+@pdseq16.init
+
+def seq16
+	return @pdseq16
 end
 
 IRB.start
 
-@sock.close
+seq16.disconnect
